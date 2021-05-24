68e199f40e26afbef833a68113cefea8d00e1c99
Benjamin Arntzen <zorlin@gmail.com>
2021-05-24 04:25:19 +0000

    Rearrange the home page

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -2,19 +2,16 @@
 
 @section('content')
     <div class="container-fluid">
-        @include('blocks.news')
-    
+        @include('blocks.featured')
+        <p></p>
         @if (!auth()->user()->chat_hidden)
             <div id="vue">
                 @include('blocks.chat')
             </div>
         @endif
-    
-        @include('blocks.featured')
         @include('blocks.poll')
         @include('blocks.top_torrents')
-        @include('blocks.top_uploaders')
-        @include('blocks.latest_topics')
+        @include('blocks.news')
         @include('blocks.latest_posts')
         @include('blocks.online')
     </div>
